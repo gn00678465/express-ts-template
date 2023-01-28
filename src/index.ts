ebe1f7d3759adb77da45420d0b34c7484c8b2f1f
Madao <gn00678465@gmail.com>
2023-01-28 21:50:50 +0800

    Add middleware package

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
 import express, { Express, Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import flash from 'connect-flash';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.SERVICE_PORT || 3000;
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({
+  secret: 'mysecretstring',
+  saveUninitialized: true,
+  resave: true,
+  cookie: {
+    httpOnly: true
+  }
+}));
+app.use(flash());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
